b12888d05956e6742d1741a43d136a92c4f75c84
dalixux <dalixux@hotmail.com>
2015-10-16 16:08:43 +0800

    update sample

--- sample/test.lua
+++ sample/test.lua
@@ -4,70 +4,92 @@ local mod = {}
 --self代表root标签
 function mod.onCreate(self)
 self:setColor(0xffff0000)
---单色
+
 local s0 = self:getChildAt(0)
+local t0 = s0:getChildAt(0)
+s0:setSize(200, 200)
 s0:setLoc(20, 20)
-s0:setSize(50, 50)
---rect
-s0:setRect(0, 0, 0, 0, 50, 50)
+s0:setRect(0, 0)
 s0:setStroke(true)
-s0:setColor(0xff00ffff)
+s0:setColor(0xff00ff00)
 
-----circle
---local s1 = self:getChildAt(1)
---s1:setLoc(80, 20)
---s1:setSize(50, 50)
---s1:setCircle(25, 25, 25)
---s1:setStroke(false)
---s1:setColor(0xffffffff)
-----line
---local s2 = self:getChildAt(2)
---s2:setLoc(140, 20)
---s2:setSize(50, 50)
---s2:setLine(0, 0, 25, 25)
---s2:setStroke(false)
---s2:setColor(0xffffffff)
-----point
+t0:setFont("Microsoft Yahei", 14, true, true, true, true)
+t0:setPosContent("bones ui中文", 20, 20, 30, 30, 40, 40, 50, 50, 60, 60, 70, 70, 80, 80, 90, 90, 100, 100, 110, 110)
+t0:setSize(198, 198)
+t0:setLoc(1, 1)
+
+local s1 = self:getChildAt(1)
+local t1 = s1:getChildAt(0)
+s1:setSize(200, 200)
+s1:setLoc(230, 20)
+s1:setRect(0, 0)
+s1:setStroke(true)
+s1:setColor(0xff00ff00)
+
+t1:setFont("Microsoft Yahei", 14, false, false, true, true)
+t1:setAutoContent("bones ui\n中\n文", 0, 0,  1)
+t1:setSize(198, 198)
+t1:setLoc(1, 1)
+
+local s2 = self:getChildAt(2)
+local t2 = s2:getChildAt(0)
+s2:setSize(200, 200)
+s2:setLoc(440, 20)
+s2:setRect(0, 0)
+s2:setStroke(true)
+s2:setColor(0xff00ff00)
+
+t2:setFont("Microsoft Yahei", 14, false, false, true, true)
+t2:setAutoContent("bones ui\n中\n文", 0, 2,  1)
+t2:setSize(198, 198)
+t2:setLoc(1, 1)
+
 local s3 = self:getChildAt(3)
-s3:setLoc(200, 20)
-s3:setSize(50, 50)
-s3:setPoint(25, 25)
-s3:setStroke(false)
-s3:setStrokeWidth(5)
-s3:setColor(0xffffffff)
-----oval
---local s4 = self:getChildAt(4)
---s4:setLoc(260, 20)
---s4:setSize(50, 150)
---s4:setOval()
---s4:setStroke(false)
---s4:setColor(0xffffffff)
-----arc
---local s5 = self:getChildAt(5)
---s5:setLoc(310, 20)
---s5:setSize(50, 150)
---s5:setArc(270, 90, true)
---s5:setStroke(false)
---s5:setColor(0xffffffff)
+local t3 = s3:getChildAt(0)
+s3:setSize(200, 200)
+s3:setLoc(650, 20)
+s3:setRect(0, 0)
+s3:setStroke(true)
+s3:setColor(0xff00ff00)
 
---local s6 = self:getChildAt(6)
---s6:setLoc(370, 20)
---s6:setSize(50, 150)
---s6:setRect(5, 5, 0, 0, 50, 150)
---s6:setStroke(true)
---s6:setColor(0xffffffff)
---s6:setStrokeEffect(2, 2, 2, 0)
+t3:setFont("Microsoft Yahei", 14, false, false, true, true)
+t3:setAutoContent("bones ui\n中\n文", 0, 1,  1)
+t3:setSize(198, 198)
+t3:setLoc(1, 1)
 
-local s8 = self:getChildAt(3)
-s8:setLoc(80, 180)
-s8:setSize(50, 50)
-s8:setCircle(25, 25, 25)
-s8:setStroke(false)
-local radial = bones.createRadialGradient(25, 25, 25, 0, 2, 0xff00ff00, 0xff00ffff, 0, 1)
-s8:setColor(radial)
-bones.destroyShader(radial)
+local s4 = self:getChildAt(4)
+local t4 = s4:getChildAt(0)
+s4:setSize(200, 200)
+s4:setLoc(20, 230)
+s4:setRect(0, 0)
+s4:setStroke(true)
+s4:setColor(0xff00ff00)
+
+t4:setFont("Microsoft Yahei", 14, true, true, true, true)
+t4:setAutoContent("bones ui\n中文abcd大师法法大赛的发生打发似的", 10, 1, 2)
+t4:setSize(198, 198)
+t4:setLoc(1, 1)
+
+local s5 = self:getChildAt(5)
+local t5 = s5:getChildAt(0)
+s5:setSize(200, 200)
+s5:setLoc(230, 230)
+s5:setRect(0, 0)
+s5:setStroke(true)
+s5:setColor(0xff00ff00)
+
+t5:setFont("Microsoft Yahei", 14, true, true, true, true)
+t5:setAutoContent("bones ui\n中文abcd大师法法大赛的发生打发似的", 10, 1, 1)
+t5:setSize(198, 198)
+t5:setLoc(1, 1)
+
+local shader = bones.createLinearGradient(0, 0, 40, 0, 2, 3, 0xffff0000, 0xff00ff00, 0xff0000ff, 0, 0.5, 1);
+t5:setColor(shader)
+bones.destroyShader(shader)
 
 end
+
+
 
 
 
